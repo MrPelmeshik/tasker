ce19f77e924d0b0a6b323c07c0e2b98fc9031b47
Ilya Fedorov <74106915+MrPelmeshik@users.noreply.github.com>
2025-09-23 14:22:25 +0300

    refactor(tasker-api): Удалил лишнее

--- src/tasker-api/TaskerApi/Interfaces/Services/IUserService.cs
+++ src/tasker-api/TaskerApi/Interfaces/Services/IUserService.cs
@@ -4,5 +4,4 @@ namespace TaskerApi.Interfaces.Services;
 
 public interface IUserService
 {
-    Task<UserEntity> GetOrCreateByWindowsIdentityAsync(string windowsIdentityName, CancellationToken cancellationToken);
 }
--- src/tasker-api/TaskerApi/Services/UserService.cs
+++ src/tasker-api/TaskerApi/Services/UserService.cs
@@ -11,26 +11,4 @@ public class UserService(
     IUserProvider userProvider)
     : IUserService
 {
-    public async Task<UserEntity> GetOrCreateByWindowsIdentityAsync(string windowsIdentityName, CancellationToken cancellationToken)
-    {
-        await using var uow = await uowFactory.CreateAsync(cancellationToken, useTransaction: true);
-
-        var existing = await userProvider.GetByNameAsync(uow.Connection, windowsIdentityName, cancellationToken, uow.Transaction);
-        if (existing != null)
-        {
-            await userProvider.UpdateAsync(uow.Connection, existing, cancellationToken, uow.Transaction, setDefaultValues: true);
-            await uow.CommitAsync(cancellationToken);
-            return existing;
-        }
-
-        var user = new UserEntity
-        {
-            Id = Guid.NewGuid(),
-            Name = windowsIdentityName
-        };
-
-        user.Id = await userProvider.CreateAsync(uow.Connection, user, cancellationToken, uow.Transaction, setDefaultValues: true);
-        await uow.CommitAsync(cancellationToken);
-        return user;
-    }
 }
