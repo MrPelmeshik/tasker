d5a772d2d34622f14762065c45c7aa36d53690fc
Ilya Fedorov <ilya-fedorov-2000@list.ru>
2026-02-13 07:54:11 +0300

    Remove migration script for adding event_date column in events table

--- docker/postgres/scripts/migrations/tasker_db/0011.add_event_occurred_at.sql
+++
@@ -1,6 +0,0 @@
--- Добавление колонки event_date в таблицу events
--- Миграция: 0011.add_event_date.sql
--- Дата события/активности (для отображения и учёта в недельной сводке)
-
-alter table events add column if not exists event_date timestamptz;
-comment on column events.event_date is 'Дата события/активности';
